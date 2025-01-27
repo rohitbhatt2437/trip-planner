382c954d47b8b3698e6d53aaa899dcf8fc1e7514
Rohit Bhatt <bhattrohed@gmail.com>
2025-01-27 19:42:09 +0530

    added some more functionality

---
+++ src/app/admin/scrape-data/page.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { apiClient } from "@/lib/api-client";
+import { ADMIN_API_ROUTES } from "@/utils";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Input,
+  Listbox,
+  ListboxItem,
+  Tab,
+  Tabs,
+} from "@heroui/react";
+import axios from "axios";
+import React, { useState } from "react";
+
+const ScrapeData = () => {
+  const [cities, setCities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState<undefined | string>(
+    undefined
+  );
+
+  const searchCities = async (searchString: string) => {
+    const response = await axios.get(
+      `https://secure.geonames.org/searchJSON?q=${searchString}&maxRows=5&username=rohit&style=SHORT`
+    );
+    const parsed = response.data?.geonames;
+    setCities(parsed?.map((city: { name: string }) => city.name) ?? []);
+  };
+
+  const startScraping = async () => {
+    await apiClient.post(ADMIN_API_ROUTES.CREATE_JOB)
+  };
+
+  return (
+    <section className="m-10 grid grid-col-3 gap-5">
+      <Card className="col-span-2">
+        <CardBody>
+          <Tabs>
+            <Tab key="location" title="location">
+              <Input
+                type="text"
+                label="search"
+                onChange={(e) => searchCities(e.target.value)}
+              />
+              <div className="w-full min-h-[200px] max-w-[260px] px-1 py-2 rounded-small border-default-200 mt-5 border">
+                <Listbox onAction={(key) => setSelectedCity(key as string)}>
+                  {cities.map((city) => (
+                    <ListboxItem
+                      key={city}
+                      color="primary"
+                      className="text-primary-500"
+                    >
+                      {city}
+                    </ListboxItem>
+                  ))}
+                </Listbox>
+              </div>
+            </Tab>
+          </Tabs>
+        </CardBody>
+        <CardFooter className="flex flex-col gap-5">
+          <div>
+            {selectedCity && (
+              <h1 className="text-xl">Scrape Data for {selectedCity}</h1>
+            )}
+          </div>
+          <Button size="lg" className="w-full" color="primary" onPress={startScraping} >
+            Scrape
+          </Button>
+        </CardFooter>
+      </Card>
+    </section>
+  );
+};
+
+export default ScrapeData;
---
+++ src/app/api/admin/create-job/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import {prisma} from '@/lib'
+
+export async function POST(request:Request){
+    try {
+        const {url, jobType} = await request.json();
+        const response = await prisma.jobs.create({data:{url,jobType}})
+        
+        return NextResponse.json({jobCreated:true}, {status:201});
+        
+    } catch (error) {
+        return NextResponse.json({message:"An unexpected error occured."}, {status:500})
+    }
+}
